e8efb3606fa78c1d65cb9832b0debb493909fedb
Vaibhav <vaibhav@maithani.com>
2020-07-20 17:40:17 +0530

    Adding Setup Vaibhav

--- EcoGreen/src/main/java/com/ecogreen/servlet/RegisterServlet.java
+++ EcoGreen/src/main/java/com/ecogreen/servlet/RegisterServlet.java
@@ -32,8 +32,7 @@ public class RegisterServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+//		WRITTEN BY VAIBHAV
 	}
 
 }
